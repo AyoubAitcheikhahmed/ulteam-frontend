319fec11e52ec636af4ce162718a356029627053
Ayoub Aitcheikhahmed <ayoub.aitcheikhahmed@win.com>
2021-12-16 13:24:43 +0100

    ERROR: CANNOT READ PROPERTY 'user' INSIDE persist:root _gefixt via een arrow-function toevoege die checkt of persist-root null is

--- ulteam/src/server_methodes.js
+++ ulteam/src/server_methodes.js
@@ -3,9 +3,25 @@ import axios from "axios";
 //import { axiosReq } from './axiosReq'
 
 const  BASE_URL = "https://ulteam-api.herokuapp.com/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).user.token;
+// let token_value = "";
+
+// if(JSON.parse(localStorage.getItem("persist:root")) == null || JSON.parse(localStorage.getItem("persist:root")).user.user == null){
+//      token_value = "Not_defined";
+// }else{
+//     token_value = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).user.token;
+// }
+
+// const TOKEN = token_value;
 
 
+const TOKEN = () => {
+    if(JSON.parse(localStorage.getItem("persist:root")) == null || JSON.parse(localStorage.getItem("persist:root")).user.user == null){
+        return "Not_defined";
+   }else{
+        return JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).user.token;
+   }
+
+}
 
 export const loginProcess = async (dispatch,user) => {
     
